bd5c338769f676c6d8b689a9fccd32a92a988a31
Archives12 <prmehta0512@gmail.com>
2026-01-30 17:45:41 +0530

    Taking Input And Printing Output

---
+++ Array/OutputAndInput.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[7];
+
+     //Taking Input 
+
+    for(int i=0 ; i<=6 ; i++){
+        cin>> arr[i];
+    }
+
+    //Printing Output 
+
+    for(int i=0 ; i<=6 ; i++){
+        cout<< arr[i] << " ";
+    }
+}
