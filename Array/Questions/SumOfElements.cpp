dc265ff23cbb0ae2b5a09bb7d7ec83d3b9ce8694
Archives12 <prmehta0512@gmail.com>
2026-01-30 19:00:23 +0530

    Sum Of All Elements in an array

---
+++ Array/Questions/SumOfElements.cpp
@@ -0,0 +1,21 @@
+// Calculate the sum of all the elements in the give array
+
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+cout<< " Enter the size of array: ";
+cin>>n;
+int arr[n];
+for (int i = 0; i <= n-1; i++)
+{
+    cin>>arr[i];
+}
+int sum = 0;
+for (int i = 0; i <= n-1; i++)
+{
+    sum = sum + arr[i];
+}
+cout<<sum;
+
+}
